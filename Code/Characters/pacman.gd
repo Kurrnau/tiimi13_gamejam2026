1c191b25bcb3bd19788214eb3503c0bbdba60001
Kurrnau <vilma_porvari@hotmail.com>
2026-05-05 08:19:23 +0300

    Pacman käännökset, muutettu node "Scores" --> "Dots"

--- Code/Characters/pacman.gd
+++ Code/Characters/pacman.gd
@@ -1,21 +1,26 @@
 class_name Pacman extends CharacterBody2D
 
-const SPEED : float = 120.0
+@export var _respawn_point : Node2D
+@export var speed : float = 120
+@onready var _animated_sprite_2d : AnimatedSprite2D = $AnimatedSprite2D
+
 const TILE_SIZE : int = 16
 
 var direction : Vector2 = Vector2.RIGHT
 var next_direction : Vector2 = Vector2.RIGHT
 
+
 func _ready() -> void:
+	# Snaps pacman into the closest 16x16 tile to make grid movement possible. 
 	position = position.snapped(Vector2(TILE_SIZE, TILE_SIZE))
 
-	
 func _physics_process(_delta: float) -> void:
 	_read_input()
 	_move()
-	# TODO: animaatiot ja hahmon kääntyminen
-	#_update_animations()
-	
+	_update_rotation()
+	# TODO: _update_animations()
+
+#region Internal Functionality	
 func _read_input() -> void:
 	if Input.is_action_pressed("Right"):
 		next_direction = Vector2.RIGHT
@@ -27,13 +32,17 @@ func _read_input() -> void:
 		next_direction = Vector2.DOWN
 
 func _is_on_grid() -> bool:
+	# Calculates pacman's position to make sure it's within the 16x16 grid.
 	var x_remainder = fmod(position.x, TILE_SIZE)
 	var y_remainder = fmod(position.y, TILE_SIZE)
+	# Error-tolerance of 2 pixels.
 	return (x_remainder < 2.0 or x_remainder > TILE_SIZE - 2.0) and \
 		   (y_remainder < 2.0 or y_remainder > TILE_SIZE - 2.0)
-	
+
 func _is_direction_blocked(dir: Vector2) -> bool:
 	var collision = move_and_collide(dir * TILE_SIZE, true)
+	# true = direction is blocked
+	# false = direction is not blocked
 	return collision != null
 
 func _move() -> void:
@@ -44,5 +53,23 @@ func _move() -> void:
 		if _is_direction_blocked(direction):
 			velocity = Vector2.ZERO
 			return
-	velocity = direction * SPEED
+	velocity = direction * speed
 	move_and_slide()
+
+func _update_rotation() -> void:
+	if direction == Vector2.RIGHT:
+		_animated_sprite_2d.rotation = 0
+		_animated_sprite_2d.flip_h = false
+	elif direction == Vector2.LEFT:
+		_animated_sprite_2d.rotation = 0
+		_animated_sprite_2d.flip_h = true
+	elif direction == Vector2.UP:
+		_animated_sprite_2d.rotation = -PI / 2
+		_animated_sprite_2d.flip_h = false
+	elif direction == Vector2.DOWN:
+		_animated_sprite_2d.rotation = PI / 2
+		_animated_sprite_2d.flip_h = false
+#endregion
+
+#func _update_animation() -> void:
+#func die()
