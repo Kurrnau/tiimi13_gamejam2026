a3c9a6146ba09012136a2af628bf0b9e26386711
WhiteSt1 <jiipekka@gmail.com>
2026-05-06 12:44:50 +0300

    Efektejä lisätty. Koodia lisätty dot ja power_up skripteihin.

--- Code/Level/dot.gd
+++ Code/Level/dot.gd
@@ -1,5 +1,7 @@
 class_name Dot extends Collectable #Area2D
 
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
 @export var _score: int = 10
 
 func collect(pacman : Pacman) -> bool:
@@ -9,3 +11,15 @@ func collect(pacman : Pacman) -> bool:
 	GameManager.add_score(_score)
 
 	return true
+
+func clear() -> void:
+	if audio_stream_player_2d == null:
+		super.clear()
+		return
+		
+	hide()
+	audio_stream_player_2d.play()
+	audio_stream_player_2d.finished.connect(_on_effect_finished)
+		
+func _on_effect_finished() -> void:
+	queue_free()
--- Code/Level/power_up.gd
+++ Code/Level/power_up.gd
@@ -2,6 +2,8 @@ class_name PowerUp extends Collectable #Area2D
 
 signal powerup_eaten					#TEST
 
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
 @export var _score: int = 50
 
 func collect(pacman : Pacman) -> bool:
@@ -15,3 +17,15 @@ func collect(pacman : Pacman) -> bool:
 
 func _on_powerup_eaten():				#TEST
 	emit_signal("powerup_eaten")
+	
+func clear() -> void:
+	if audio_stream_player_2d == null:
+		super.clear()
+		return
+		
+	hide()
+	audio_stream_player_2d.play()
+	audio_stream_player_2d.finished.connect(_on_effect_finished)
+		
+func _on_effect_finished() -> void:
+	queue_free()
